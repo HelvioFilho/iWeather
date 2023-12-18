4920d0ee3e14897f1fc5ccff64f8ea6dfc708732
Hélvio Filho <helviosvf@gmail.com>
2023-12-18 19:40:39 -0300

    feat: created project

---
+++ src/constants/Colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  white: "#FFFFFF",
+  blue: {
+    light: "#8FB2F5",
+  },
+  gray: {
+    900: "#13131A",
+    800: "#16161F",
+    700: "#1C1C27",
+    600: "#22222F",
+    500: "#3B3B54",
+    400: "#7F7F98",
+    300: "#ABABC4",
+    200: "#BFBFD4",
+    100: "#FAFAFA",
+  },
+};
