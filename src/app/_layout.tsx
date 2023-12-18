4b92ec8521cda410e581cef3df7568f019ff64fc
Hélvio Filho <helviosvf@gmail.com>
2023-12-18 20:15:04 -0300

    feat: installed and configured nunito font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -7,9 +14,30 @@ export const unstable_settings = {
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-// SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <RootLayoutNav />;
 }
 
