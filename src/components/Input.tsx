e5120a9108fa0cfa659915b6265f3c764a7e16b9
Hélvio Filho <helviosvf@gmail.com>
2023-12-18 22:14:18 -0300

    feat: created input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import {
+  View,
+  TextInput,
+  TextInputProps,
+  ActivityIndicator,
+} from "react-native";
+
+import { colors } from "@/constants/Colors";
+
+type InputProps = TextInputProps & {
+  isLoading?: boolean;
+};
+
+export function Input({ isLoading = false, ...rest }: InputProps) {
+  return (
+    <View className="w-full h-14 bg-gray-600 rounded-lg px-5 flex-row">
+      <TextInput
+        className="flex-1 font-regular text-base text-white"
+        placeholderTextColor={colors.gray[400]}
+        {...rest}
+      />
+
+      {isLoading && <ActivityIndicator color={colors.blue.light} />}
+    </View>
+  );
+}
