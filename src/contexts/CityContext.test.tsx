93bbda127fb5a650e14e361dbcfbda703d80024d
Hélvio Filho <helviosvf@gmail.com>
2023-12-20 11:59:57 -0300

    test: checking the functionality of the useCity hook

--- src/contexts/CityContext.test.tsx
+++ src/contexts/CityContext.test.tsx
@@ -7,7 +7,10 @@ describe("Context: cityContext", () => {
   it("should be change selected city", async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
 
+    // waitFor aguarda a função async ser concluída
     await waitFor(() =>
+      // lida com o useState e o useEffect
+      // o act garante que a renderização seja feita dentro do contexto
       act(() =>
         result.current.handleChanceCity({
           id: "1",
